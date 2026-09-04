0b4c9530699e6f18f36cc70a3c8b131e90c6361e
kevin tanes <82017999+kevintanes@users.noreply.github.com>
2026-09-04 21:25:44 +0700

    [FE-15] ADMIN DASHBOARD LAYOUT

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "./pages/home";
 import RegisterPage from "./pages/register";
 import NotfoundPage from "./pages/not-found";
@@ -14,34 +14,53 @@ import BookingDetailPage from "./pages/booking-detail";
 import DashboardPage from "./pages/dashboard";
 import GuestRoute from "./helper/GuestRoute";
 import ProtectedRoute from "./helper/ProtectedRoute";
+import AdminLayout from "./components/AdminLayout";
+import AdminDashboardPage from "./pages/admin";
+
+const AppContent = () => {
+  const location = useLocation();
+  const isAdminRoute = location.pathname.startsWith("/admin");
+
+  return (
+    <>
+      <ScrollToTop />
+      <AuthProvider>
+        <Toaster />
+        {!isAdminRoute && <Navbar />}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/services/:id" element={<ServiceDetailPage />} />
+
+          <Route element={<GuestRoute />}>
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Route>
+
+          <Route element={<ProtectedRoute />}>
+            <Route path="/services/:id/book" element={<BookPage />} />
+            <Route path="/booking/:id" element={<BookingDetailPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+          </Route>
+
+          <Route path="/admin" element={<ProtectedRoute isAdminOnly={true} />}>
+            <Route element={<AdminLayout />}>
+              <Route index element={<AdminDashboardPage />} />
+            </Route>
+          </Route>
+
+          <Route path="*" element={<NotfoundPage />} />
+        </Routes>
+      </AuthProvider>
+    </>
+  );
+};
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <ScrollToTop />
-        <AuthProvider>
-          <Toaster />
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/services" element={<ServicesPage />} />
-            <Route path="/services/:id" element={<ServiceDetailPage />} />
-
-            <Route element={<GuestRoute />}>
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/login" element={<LoginPage />} />
-            </Route>
-
-            <Route element={<ProtectedRoute />}>
-              <Route path="/services/:id/book" element={<BookPage />} />
-              <Route path="/booking/:id" element={<BookingDetailPage />} />
-              <Route path="/dashboard" element={<DashboardPage />} />
-            </Route>
-
-            <Route path="*" element={<NotfoundPage />} />
-          </Routes>
-        </AuthProvider>
+        <AppContent />
       </BrowserRouter>
     </>
   );
